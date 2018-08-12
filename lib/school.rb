f3476303872390db7462582e9074e4f2eb14ba3c
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-12 23:17:21 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,14 @@
 # code here!
 class School
 attr_accessor :student_name, :student_grade
-attr_reader :school_name, :roster
+attr_reader :school_name
 
   def initialize(school_name)
     @school_name=school_name
+    roster
   end
 
-  def roster=(roster)
+  def roster
     @roster={}
   end
 
