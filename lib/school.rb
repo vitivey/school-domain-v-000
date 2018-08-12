2feb987ec0d4fede1fa65a5b0f74897f1166f9a3
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-12 23:18:32 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,7 +12,7 @@ attr_reader :school_name
     @roster={}
   end
 
-  def add_student(student_grade, student_name)
+  def add_student(student_name, student_grade)
     @roster[student_grade] = [] #if !roster.keys.include?(student_grade)
     @roster[student_grade] << student_name
   end
