705a0f88c0928f84a782c1a95f14842c6caf27f8
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-12 23:13:26 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,7 +8,7 @@ attr_reader :school_name
     @roster={}
   end
 
-  def add_student(student_name, student_grade)
-    @sc
+  def add_student(student_grade, student_name)
+    @roster[student_grade] << student_name
   end
 end
